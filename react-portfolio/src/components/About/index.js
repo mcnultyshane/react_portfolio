5303ec06a492028cf120331eb3657feb17a91e92
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-04 14:52:16 -0400

    added about folder and contents to index.js

---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section className="container">
+            <h2 class="top-title">Shane McNulty</h2>
+            <hr></hr>
+            <div>
+                <div className="container photo-container">
+                    <div className="card card0">
+                        <div className="border">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>   
+            My name is Shane McNulty and I am currently enrolled in the University of Penn's Coding Boot Camp. I am learning full-stack development with a focus on Javascript, CSS, HTML, MySQL and now moving into Mongo and React. This Portfolio is designed to highlight some of my work in this course as I transition into Coding and Digitial Language from 20 years in the hospiality industry. 
+            <hr></hr>
+            I am a driven worker with 10 years of management experience. I am an excellent communicator that thrives in problem solving and attaining set goals. I have listed below a set of links that will take you to the projects that I have completed or I am currently working on. I have also listedmy contact information below. Please feel free to reach out at any time if you have any questions about my work or would like to speak further. Thank you for visiting.
+            </p>
+        </section>
+    )
+}
+
+export default About;
