8263483326fd431752485f7d61833dbc5029cbdd
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-04 23:11:25 -0400

    adding google fonts and updating styling.

--- react-portfolio/src/components/Pages/Projects.js
+++ react-portfolio/src/components/Pages/Projects.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Individual Cards
 
 function ProjectCards(props) {
-    return (
-        <div className="card">
-            <div className="img-container">
-                <img class="img-thumbnail" alt={props.name} src={props.image}/>
-            </div>
-            <div className="content">
-                <p className="card-title">{props.name}</p>
-                <p class="proj-icons-container">
-
-                </p>
-                <p class="topics">
-                    {props.topics}
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img class="img-thumbnail" alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <p className="card-title">{props.name}</p>
+        <p class="topics">{props.topics}</p>
+        <p class="proj-icons-container">
+          <a href={props.github}>
+            <img
+              class="project-icons"
+              src="https://img.icons8.com/material-rounded/48/000000/github.png"
+              alt="Github Icon"
+            />{" "}
+          </a>
+          <a href={props.deploy}>
+            <img
+              class="project-icons"
+              src="https://img.icons8.com/material-outlined/48/000000/link--v1.png"
+              alt="Link Icon"
+            />{" "}
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 export default ProjectCards;
