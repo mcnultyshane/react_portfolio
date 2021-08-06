4f9835dc9cd46ea8b59a131d88f91ba1eefe84ad
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-05 21:50:05 -0400

    changed component name

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-// import Header from './components/Header';
-import PortfolioContainer from './components/PortfolioContainer';
+import Header from './components/Header';
 import Footer from './components/Footer';
-// import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <PortfolioContainer />
+     
+      <Header />
       <Footer />
     </div>
   );
