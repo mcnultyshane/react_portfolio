30c67e991962e73e38c6bf5a9528acdbd1c368ba
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-08-05 21:43:33 -0400

    added about me

--- react-portfolio/src/components/Pages/About.js
+++ react-portfolio/src/components/Pages/About.js
@@ -18,6 +18,8 @@ function About() {
             My name is Shane McNulty and I am currently enrolled in the University of Penn's Coding Boot Camp. I am learning full-stack development with a focus on Javascript, CSS, HTML, MySQL and now moving into Mongo and React. This Portfolio is designed to highlight some of my work in this course as I transition into Coding and Digitial Language from 20 years in the hospiality industry. 
             <hr></hr>
             I am a driven worker with 10 years of management experience. I am an excellent communicator that thrives in problem solving and attaining set goals. Please click the 'Project' tabs in the naviagation bar to see a group of projects that I have completed or I am currently working on. Please feel free to reach out at any time if you have any questions about my work or would like to speak further. Thank you for visiting.
+            <hr></hr>
+            Click on the headshot and say "Hello" to my partner, Faith.
             </p>
             </div>
         </section>
